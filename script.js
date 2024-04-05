3c11cdebf84ec0d62984783b2596aa72e9161816
MATHEUS SANTOS PEREIRA <021.898669@senaicimatec.edu.br>
2024-04-05 15:13:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Exemplo de função para adicionar produtos ao carrinho
+function adicionarAoCarrinho(produto) {
+    // Lógica para adicionar o produto ao carrinho
+    console.log(`Produto "${produto}" adicionado ao carrinho.`);
+}
+
+// Exemplo de uso da função
+adicionarAoCarrinho('Pão Integral');
